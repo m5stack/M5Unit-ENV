443c9d7d3be9cc409878bb3b3ca80d2f81419aac
TinyuChiu <TInyu.Zhao@gmail.com>
2023-02-06 11:06:09 +0800

    add Hat ENVIII

--- src/SHT3X.cpp
+++ src/SHT3X.cpp
@@ -4,8 +4,27 @@
 
 */
 SHT3X::SHT3X(uint8_t address) {
-    Wire.begin();
     _address = address;
+}
+
+bool SHT3X::detectDevice() {
+    unsigned int data[6];
+    Serial.println(Wire.begin(0, 26));
+    Wire.beginTransmission(_address);
+    Wire.write(0x2C);
+    Wire.write(0x06);
+    // Stop I2C transmission
+    Wire.endTransmission();
+    Wire.requestFrom(_address, (uint8_t)1);
+    for (int i = 0; i < 6; i++) {
+        data[i] = Wire.read();
+        Serial.printf("data%d:%d ", i, data[i]);
+    };
+    if (data[1] == -1) {
+        Wire.begin();
+        return 1;
+    }
+    return 0;
 }
 
 byte SHT3X::get() {
