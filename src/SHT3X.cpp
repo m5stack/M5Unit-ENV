731764f730608ba913c08e5836d023586a5d3d33
Sergio <massesos@gmail.com>
2023-05-07 11:36:51 +0100

    Added OneWire Support to SHT3X
    Added the possibility to select another I2C Bus for SHT3X, inline to what it is already implemented for QMP6988

--- src/SHT3X.cpp
+++ src/SHT3X.cpp
@@ -1,70 +1,45 @@
 #include "SHT3X.h"
 
-/* Motor()
-
-*/
-SHT3X::SHT3X(uint8_t address, uint8_t deviceType) {
-    if (deviceType == 0)
-        Wire.begin();
-    else if (deviceType == 1)
-        Wire.begin(0, 26);
-    else if (deviceType == 2)
-        Wire.begin(2, 1);
-    _address = address;
+bool SHT3X::init(uint8_t slave_addr_in, TwoWire* wire_in)
+{
+  _wire = wire_in;
+  _address=slave_addr_in;
+  return true;
 }
 
-// /*! @Detecting whether a device is a Unit or a Hat
-//     @return Return 0 for Unit, 1 for Hat */
-// bool SHT3X::detectDevice() {
-//     unsigned int data[6];
-//     Wire.begin();
-//     Wire.beginTransmission(_address);
-//     Wire.write(0x2C);
-//     Wire.write(0x06);
-//     // Stop I2C transmission
-//     Wire.endTransmission();
-//     Wire.requestFrom(_address, (uint8_t)1);
-//     for (int i = 0; i < 6; i++) {
-//         data[i] = Wire.read();
-//         Serial.printf("data%d:%d ", i, data[i]);
-//     };
-//     if (data[1] == -1) {
-//         Wire.begin(0, 26);
-//         return 1;
-//     }
-//     return 0;
-// }
+byte SHT3X::get()
+{
+  unsigned int data[6];
 
-byte SHT3X::get() {
-    unsigned int data[6];
+  // Start I2C Transmission
+  _wire->beginTransmission(_address);
+  // Send measurement command
+  _wire->write(0x2C);
+  _wire->write(0x06);
+  // Stop I2C transmission
+  if (_wire->endTransmission()!=0) 
+    return 1;  
 
-    // Start I2C Transmission
-    Wire.beginTransmission(_address);
-    // Send measurement command
-    Wire.write(0x2C);
-    Wire.write(0x06);
-    // Stop I2C transmission
-    if (Wire.endTransmission() != 0) return 1;
+  delay(200);
 
-    delay(200);
+  // Request 6 bytes of data
+  _wire->requestFrom(_address, (uint8_t) 6);
 
-    // Request 6 bytes of data
-    Wire.requestFrom(_address, (uint8_t)6);
+  // Read 6 bytes of data
+  // cTemp msb, cTemp lsb, cTemp crc, humidity msb, humidity lsb, humidity crc
+  for (int i=0;i<6;i++) {
+    data[i]=_wire->read();
+  };
+  
+  delay(50);
+  
+  if (_wire->available()!=0) 
+    return 2;
 
-    // Read 6 bytes of data
-    // cTemp msb, cTemp lsb, cTemp crc, humidity msb, humidity lsb, humidity crc
-    for (int i = 0; i < 6; i++) {
-        data[i] = Wire.read();
-    };
+  // Convert the data
+  cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45;
+  fTemp = (cTemp * 1.8) + 32;
+  humidity = ((((data[3] * 256.0) + data[4]) * 100) / 65535.0);
 
-    delay(50);
-
-    if (Wire.available() != 0) return 2;
-
-    // Convert the data
-    cTemp    = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45;
-    fTemp    = (cTemp * 1.8) + 32;
-    humidity = ((((data[3] * 256.0) + data[4]) * 100) / 65535.0);
-
-    return 0;
+  return 0;
 }
