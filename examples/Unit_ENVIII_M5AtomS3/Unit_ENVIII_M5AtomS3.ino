19aeb1a3df68919256f0347891912bd541974038
Tinyu <Tinyu.Zhao@gmail.com>
2024-07-18 09:43:29 +0800

    Fixed ENVIII M5Atom series bug

--- examples/Unit_ENVIII_M5AtomS3/Unit_ENVIII_M5AtomS3.ino
+++ examples/Unit_ENVIII_M5AtomS3/Unit_ENVIII_M5AtomS3.ino
@@ -1,12 +1,12 @@
 /**
- * @file ENV_II.ino
+ * @file ENV_III.ino
  * @author SeanKwok (shaoxiang@m5stack.com)
- * @brief 
- * @version 0.1
- * @date 2024-01-30
+ * @brief
+ * @version 0.2
+ * @date 2024-07-18
  *
  *
- * @Hardwares: M5Core + Unit ENV_II
+ * @Hardwares: M5AtomS3 + Unit ENV_III
  * @Platform Version: Arduino M5Stack Board Manager v2.1.0
  * @Dependent Library:
  * M5UnitENV: https://github.com/m5stack/M5Unit-ENV
@@ -15,26 +15,23 @@
 #include "M5UnitENV.h"
 
 SHT3X sht3x;
-BMP280 bmp;
+QMP6988 qmp;
 
 void setup() {
     Serial.begin(115200);
+    if (!qmp.begin(&Wire, QMP6988_SLAVE_ADDRESS_L, 2, 1, 400000U)) {
+        while (1) {
+            Serial.println("Couldn't find QMP6988");
+            delay(500);
+        }
+    }
 
     if (!sht3x.begin(&Wire, SHT3X_I2C_ADDR, 2, 1, 400000U)) {
-        Serial.println("Couldn't find SHT3X");
-        while (1) delay(1);
+        while (1) {
+            Serial.println("Couldn't find SHT3X");
+            delay(500);
+        }
     }
-
-    if (!bmp.begin(&Wire, BMP280_I2C_ADDR, 2, 1, 400000U)) {
-        Serial.println("Couldn't find BMP280");
-        while (1) delay(1);
-    }
-    /* Default settings from datasheet. */
-    bmp.setSampling(BMP280::MODE_NORMAL,     /* Operating Mode. */
-                    BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                    BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                    BMP280::FILTER_X16,      /* Filtering. */
-                    BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void loop() {
@@ -49,16 +46,16 @@ void loop() {
         Serial.println("-------------\r\n");
     }
 
-    if (bmp.update()) {
-        Serial.println("-----BMP280-----");
+    if (qmp.update()) {
+        Serial.println("-----QMP6988-----");
         Serial.print(F("Temperature: "));
-        Serial.print(bmp.cTemp);
-        Serial.println(" degrees C");
+        Serial.print(qmp.cTemp);
+        Serial.println(" *C");
         Serial.print(F("Pressure: "));
-        Serial.print(bmp.pressure);
+        Serial.print(qmp.pressure);
         Serial.println(" Pa");
         Serial.print(F("Approx altitude: "));
-        Serial.print(bmp.altitude);
+        Serial.print(qmp.altitude);
         Serial.println(" m");
         Serial.println("-------------\r\n");
     }
